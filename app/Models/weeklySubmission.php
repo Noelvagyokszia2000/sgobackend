205774626fd1048f2b4266bf3783ef5204bc2654
Noelvagyokszia2000 <noelvok123456@gmail.com>
2026-05-14 13:06:56 +0200

    SGO backend 3. commit

--- app/Models/weeklySubmission.php
+++ app/Models/weeklySubmission.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class WeeklySubmission extends Model
 {
+    protected $table = 'weeklySubmissions';
     public $timestamps = false;
 
     protected $fillable = [
